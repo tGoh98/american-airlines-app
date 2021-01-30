fddc383709b915d8ad1b12461751a8cc5ca782cc
tGoh98 <tmg5@rice.edu>
2021-01-30 12:14:09 -0600

    Initial Commit

---
+++ american-airlines-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  american-airlines-app
+//
+//  Created by Timothy Goh on 1/30/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ american-airlines-app/american_airlines_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  american_airlines_appApp.swift
+//  american-airlines-app
+//
+//  Created by Timothy Goh on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct american_airlines_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
